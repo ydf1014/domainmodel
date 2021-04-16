7ae3e949e1c0c45fce1ff2b26880a39cc1525942
Danfeng Yang <ydf1014@uw.edu>
2021-04-15 18:03:54 -0700

    Initial commit

---
+++ Sources/DomainModel/DomainModel.swift
@@ -0,0 +1,34 @@
+struct DomainModel {
+    var text = "Hello, World!"
+        // Leave this here; this value is also tested in the tests,
+        // and serves to make sure that everything is working correctly
+        // in the testing harness and framework.
+}
+
+////////////////////////////////////
+// Money
+//
+public struct Money {
+}
+
+////////////////////////////////////
+// Job
+//
+public class Job {
+    public enum JobType {
+        case Hourly(Double)
+        case Salary(UInt)
+    }
+}
+
+////////////////////////////////////
+// Person
+//
+public class Person {
+}
+
+////////////////////////////////////
+// Family
+//
+public class Family {
+}
